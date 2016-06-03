b2dc0d032d442af919d85d7be1ae865a379e544a
A A <A A>
2016-06-03 16:15:51 -0400

    Directed Graph updates

--- src/ExplicitTypedSelfReferences/Graph/DirectedGraph.scala
+++ src/ExplicitTypedSelfReferences/Graph/DirectedGraph.scala
@@ -4,11 +4,13 @@ package ExplicitTypedSelfReferences.Graph
   * Created by scuanieg on 6/3/2016.
   */
 abstract class DirectedGraph extends Graph {
+
   type Edge <: EdgeImpl
   class EdgeImpl(origin: Node, dest: Node) {
     def from = origin
     def to = dest
   }
+
   class NodeImpl extends NodeIntf {
     self: Node =>
     def connectWith(node: Node): Edge = {
@@ -17,14 +19,17 @@ abstract class DirectedGraph extends Graph {
       edge
     }
   }
+
   protected def newNode: Node
   protected def newEdge(from: Node, to: Node): Edge
   var nodes: List[Node] = Nil
   var edges: List[Edge] = Nil
+
   def addNode: Node = {
     val node = newNode
     nodes = node :: nodes
     node
   }
+
 }
 
